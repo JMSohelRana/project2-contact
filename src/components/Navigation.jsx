1565566c0b742d1f4e5629c6dba74c74537819b2
JMSohelRana <jmsohelrana04@gmail.com>
2024-01-25 16:27:32 +0600

    navigative styles

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import logo from "/public/images/contact.png";
+import "../components/css/Navigation.css";
+const Navigation = () => {
+  return (
+    <nav className=" container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
